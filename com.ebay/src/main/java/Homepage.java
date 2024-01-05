dca72f82998ad083870a3bb06fe697131a32d1c3
hanifarif81 <arifhanif2815@gmail.com>
2024-01-04 23:59:51 -0500

    2nd  commit on ebay branch to see it works

--- com.ebay/src/main/java/Homepage.java
+++ com.ebay/src/main/java/Homepage.java
@@ -1,5 +1,12 @@
 public class Homepage {
     public static void main(String[] args) {
         System.out.println("hellow world");
+        Homepage homepage = new Homepage();
+        homepage.ebaycalculate();
+
+    }
+
+    public void ebaycalculate(){
+        System.out.println("ebay calculate");
     }
 }
